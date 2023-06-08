372fe4a897591e61daba27f1e337bf0e3765d453
m6hdix <m6hdixx@gmail.com>
2023-06-08 08:29:33 +0430

    ✅ 🔨 :fix cors

--- src/Helper/Request.js
+++ src/Helper/Request.js
@@ -2,5 +2,8 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: "https://react-mini-projects-api.classbon.com",
-  headers: { "Access-Control-Allow-Origin": "*" },
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Content-Type': 'application/json',
+  },
 });
