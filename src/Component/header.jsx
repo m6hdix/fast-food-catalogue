7a366e45b4ac1268f1eda1d79f6fe60bc592aa89
m6hdix <m6hdixx@gmail.com>
2023-06-09 08:46:53 +0430

    ✅ 🔨 :fix style

--- src/Component/header.jsx
+++ src/Component/header.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
-import ImgHeader from '../assets/header.jpg';
+import React from "react";
+import ImgHeader from "../assets/header.jpg";
 const header = () => {
-    return (
-        <div className='w-full  h-48  relative bg-cover bg-center	' style={{backgroundImage:`url(${ImgHeader})`}}>
-            <h1  className='text-3xl   md:text-5xl uppercase absolute text-white left-1/4 top-1/3'>Fast Food Catalogue</h1>
-        </div>
-    );
+  return (
+    <div
+      className="w-full  h-48  relative bg-cover bg-right md:bg-center	"
+      style={{ backgroundImage: `url(${ImgHeader})` }}
+    >
+      <h1 className="text-2xl whitespace-nowrap  md:text-5xl uppercase absolute text-white right-[10%]  md:left-1/4 top-1/3">
+        Fast Food Catalogue
+      </h1>
+    </div>
+  );
 };
 
 export default header;
