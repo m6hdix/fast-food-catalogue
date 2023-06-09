b9126198656847dd359a83f1023051f690d0b37b
m6hdix <m6hdixx@gmail.com>
2023-06-09 08:27:46 +0430

    ✅ 🔨 :fix style

--- src/Component/ProductCart.jsx
+++ src/Component/ProductCart.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const ProductCart = ({ fastFood }) => {
   return (
     <div className="card card-side bg-base-100 shadow-xl h-60  ">
-      <img className="aspect-square w-40 object-cover" src={fastFood?.imageUrl} alt="" />
-      <div className="card-body">
+      <img className="aspect-square w-20 sm:w-2/5 object-cover " src={fastFood?.imageUrl} alt="" />
+      <div className="card-body p-4 md:p-6">
         <h2 className="card-title">{fastFood?.name}</h2>
         <p>{fastFood?.ingredients}</p>
         <div className="card-actions justify-end">
